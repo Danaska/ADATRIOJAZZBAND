8f25073b1fef00c5ea3afc232c5fc9804f3b29b5
danaska <danaska.alonso@gmail.com>
2022-09-12 13:34:31 +0200

    Fixes

--- adatriojazzband-website/src/components/pages/Contacto.js
+++ adatriojazzband-website/src/components/pages/Contacto.js
@@ -2,5 +2,9 @@ import React from 'react';
 import '../../App.css';
 
 export default function Contacto() {
-    return <h1 className='contacto'>Contacto</h1>;
+    return (
+    <>
+    <h1 className='contacto'>Contacto</h1>
+    </>
+    )
 }
